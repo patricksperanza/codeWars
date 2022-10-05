e0c8c8c9779fbcb6a1beb715215e92565825863e
Patrick Speranza <patrick.m.speranza@gmail.com>
2022-10-05 06:32:02 -0500

    added 8kyu challenges

---
+++ 8kyu/switch-it-up.js
@@ -0,0 +1,14 @@
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
+
+function switchItUp(number){
+    let list = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+    return list[number]
+  }
+  
+  
