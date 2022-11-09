bc9062c886e6f71cf3ed567a08d182f2ea7b0153
Patrick Speranza <patrick.m.speranza@gmail.com>
2022-11-08 20:26:34 -0600

    added 7kyu

---
+++ 7kyu/remove-anchor.js
@@ -0,0 +1,11 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+function removeUrlAnchor(url) {
+  let split = url.split("#");
+  return split[0];
+}
