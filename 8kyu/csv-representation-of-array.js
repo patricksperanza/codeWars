ef784a1adabd3fb8cea320fdbd5124b657093c1e
Patrick Speranza <patrick.m.speranza@gmail.com>
2022-11-16 19:47:53 -0600

    added 8kyu

---
+++ 8kyu/csv-representation-of-array.js
@@ -0,0 +1,24 @@
+// Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+// Example:
+
+// input:
+//    [[ 0, 1, 2, 3, 4 ],
+//     [ 10,11,12,13,14 ],
+//     [ 20,21,22,23,24 ],
+//     [ 30,31,32,33,34 ]]
+
+// output:
+//      '0,1,2,3,4\n'
+//     +'10,11,12,13,14\n'
+//     +'20,21,22,23,24\n'
+//     +'30,31,32,33,34'
+// Array's length > 2.
+
+function toCsvText(array) {
+  let result = "";
+  for (let i = 0; i < array.length; i++) {
+    result += array[i].join(",") + "\n";
+  }
+  return result.slice(0, result.length - 1);
+}
