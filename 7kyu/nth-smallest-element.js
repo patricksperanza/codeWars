759f3a7727c233793b5dff59d4120bd03ae7b0a4
Patrick Speranza <patrick.m.speranza@gmail.com>
2023-02-03 21:24:13 -0600

    added 7kyu

---
+++ 7kyu/nth-smallest-element.js
@@ -0,0 +1,12 @@
+// Task
+// Given an array/list of integers, find the Nth smallest element in the array.
+
+// Notes
+// Array/list size is at least 3.
+// Array/list's numbers could be a mixture of positives , negatives and zeros.
+// Repetition in array/list's numbers could occur, so don't remove duplications.
+
+function nthSmallest(arr, pos) {
+  arr.sort((a, b) => a - b)
+  return arr[pos - 1]
+}
