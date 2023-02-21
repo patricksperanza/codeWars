c69a6e7d913e43ce5a8dbb578618012856980aab
Patrick Speranza <patrick.m.speranza@gmail.com>
2023-02-21 08:07:26 -0600

    7kyu array leaders

--- 8kyu/crash-override.js
+++ 8kyu/crash-override.js
@@ -1,3 +1,21 @@
+// Every budding hacker needs an alias! The Phantom Phreak, Acid Burn, Zero Cool and Crash Override are some notable examples from the film Hackers.
+
+// Your task is to create a function that, given a proper first and last name, will return the correct alias.
+
+// Notes:
+// Two objects that return a one word name in response to the first letter of the first name and one for the first letter of the surname are already given. See the examples below for further details.
+
+// If the first character of either of the names given to the function is not a letter from A - Z, you should return "Your name must start with a letter from A - Z."
+
+// Sometimes people might forget to capitalize the first letter of their name so your function should accommodate for these grammatical errors.
+
+// Examples
+// // These two objects are preloaded, you need to use them in your code
+// var firstName = {A: 'Alpha', B: 'Beta', C: 'Cache' ...}
+// var surname = {A: 'Analogue', B: 'Bomb', C: 'Catalyst' ...}
+
+// aliasGen('Larry', 'Brentwood') === 'Logic Bomb'
+
 function aliasGen(first, last) {
   const firstLet = first[0].toUpperCase()
   const lastLet = last[0].toUpperCase()
