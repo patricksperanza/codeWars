092e7e8cde7b007ce947242f4b3adcaf30c7b2d9
Patrick Speranza <patrick.m.speranza@gmail.com>
2023-03-09 07:54:05 -0600

    7kyu squares sequence

---
+++ 7kyu/squares-sequence.js
@@ -0,0 +1,12 @@
+// Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+
+// Examples
+// 2, 5  -->  [2, 4, 16, 256, 65536]
+// 3, 3  -->  [3, 9, 81]
+
+function squares(x, n) {
+  if (n <= 0) return []
+  const res = [x]
+  for (let i = 1; i < n; i++) res.push(res[res.length - 1] ** 2)
+  return res
+}
